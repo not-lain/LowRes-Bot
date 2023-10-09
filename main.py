b994491fbf55d68f79f31ac6f93c442af5e04b5a
hafedh-hichri <h.hichri@instadeep.com>
2023-10-09 03:58:56 +0100

    added echo and meme commands

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import io
 import discord
 from settings import TOKEN
+import aiohttp
+
 bot = discord.Bot()
 
 
@@ -10,6 +13,31 @@ async def on_ready():
 @bot.slash_command(name='ping',description='replies to the bot ping')
 async def ping(ctx):
     await ctx.respond(f'Pong! {round(bot.latency * 1000)}ms')
+
+#echo command
+@bot.slash_command(name='echo',description='replies with the same message')
+async def echo(ctx,*,message):
+    await ctx.respond(message)
+
+
+
+# meme command
+@bot.slash_command(name='meme',description='search the web for a meme image')
+async def meme(ctx,text1:str,text2:str):
+    if text1 == None or text2 == None:
+        return await ctx.respond('please provide 2 texts')
+    
+    meme_name = f'{text1}/{text2}'
+    d = {' ': '_', '/': '~s', '?': '~q', '%': '~p', '#': '~h', '+': '~p', '"': "''"}
+    for k, v in d.items():
+        meme_name = meme_name.replace(k, v)
+
+    meme_url = f'https://memegen.link/touch/{meme_name}/.jpg'
+    await ctx.respond(meme_url)
+
+    
+    
+    
 
 
 bot.run(TOKEN)
