e9f579d0d48a04247ef8b5496e9dc5e540c40c15
hafedh-hichri <h.hichri@instadeep.com>
2023-10-10 10:42:23 +0100

    added a module to ask AI

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import io
 import discord
 from settings import TOKEN
 import aiohttp
-
+from gradio_client import Client
+import time
 bot = discord.Bot()
 
+client = Client("tiiuae/falcon-180b-demo")
+def predict(text):
+    return client.predict(text,"", 0.9, 256, 0.95, 1.0)
 
 @bot.event
 async def on_ready():
@@ -27,18 +31,22 @@ async def meme(ctx,text1:str,text2:str):
     if text1 == None or text2 == None:
         return await ctx.respond('please provide 2 texts')
     
-    
     d = {' ': '_', '/': '~s', '?': '~q', '%': '~p', '#': '~h', '+': '~p', '"': "''"}
     for k, v in d.items():
         text1 = text1.replace(k, v)
         text2 = text2.replace(k, v)
     meme_name = f'{text1}/{text2}'
-    
 
     meme_url = f'https://memegen.link/touch/{meme_name}/.jpg'
     await ctx.respond(meme_url)
 
-    
+
+# AI prediction command
+@bot.slash_command(name='ask',description='ask falcon-180b-demo AI')
+async def ask(ctx,*,question):
+    await ctx.respond('thinking...')
+    prediction = predict(question)
+    await ctx.respond(prediction)
     
     
 
