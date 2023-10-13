5cad312e1c7cae13bcd1be401a8ac7bfd1c67ba4
lain-making-bread <hhichri60@gmail.com>
2023-10-14 00:19:21 +0100

    removed the meme command

--- main.py
+++ main.py
@@ -25,22 +25,6 @@ async def echo(ctx,*,message):
 
 
 
-# meme command
-@bot.slash_command(name='meme',description='search the web for a meme image')
-async def meme(ctx,text1:str,text2:str):
-    if text1 == None or text2 == None:
-        return await ctx.respond('please provide 2 texts')
-    
-    d = {' ': '_', '/': '~s', '?': '~q', '%': '~p', '#': '~h', '+': '~p', '"': "''"}
-    for k, v in d.items():
-        text1 = text1.replace(k, v)
-        text2 = text2.replace(k, v)
-    meme_name = f'{text1}/{text2}'
-
-    meme_url = f'https://memegen.link/touch/{meme_name}/.jpg'
-    await ctx.respond(meme_url)
-
-
 # AI prediction command
 @bot.slash_command(name='ask',description='ask falcon-180b-demo AI')
 async def ask(ctx,*,question):
@@ -50,8 +34,6 @@ async def ask(ctx,*,question):
         await ctx.respond(prediction)
     except Exception as e: 
         await ctx.respond(e)
-    
-# bot.run(TOKEN)
 
 
 app = FastAPI()
